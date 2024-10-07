49ef4c3fb8667a8722c34a2daaf7f2922952652d
SuNsTr1kE <gbogdan654@gmail.com>
2024-10-07 23:14:54 +0300

    /

--- start-react/src/App.jsx
+++ start-react/src/App.jsx
@@ -1,4 +1,6 @@
 import Header from "./components/Header"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.';
 
 function App() {
   // const [count, setCount] = useState(0)
